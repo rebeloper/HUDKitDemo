478a891e35d8b31fef265025d5c9e9023033df1f
Alex Nagy <therebeloper@gmail.com>
2020-12-25 18:49:39 +0200

    0.0.3

--- HUDKitDemo/ContentView.swift
+++ HUDKitDemo/ContentView.swift
@@ -6,11 +6,40 @@
 //
 
 import SwiftUI
+import HUDKit
 
 struct ContentView: View {
+    
+    @StateObject var hudManager = HUDManager()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(spacing: 12) {
+            Text("HUD Kit Demo").font(.title)
+            
+            Button("Show HUD") {
+                hudManager.show("Working...")
+                
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                    hudManager.update("Still working hard...")
+                }
+                
+                DispatchQueue.main.asyncAfter(deadline: .now() + 4) {
+                    hudManager.update("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.")
+                }
+                
+                DispatchQueue.main.asyncAfter(deadline: .now() + 7) {
+                    hudManager.update("Almost done...")
+                }
+                
+                DispatchQueue.main.asyncAfter(deadline: .now() + 8) {
+                    hudManager.hide("Going away 👋")
+                }
+            }
+            
+            Spacer()
+        }
+        .padding()
+        .uses(hudManager, options: HUDOptions(hudHorizontalPadding: 32, blurRadius: 1))
     }
 }
 
