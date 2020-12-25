9ac0e887f1245ec5189b26f340d36d69a6d0dc8a
Alex Nagy <therebeloper@gmail.com>
2020-12-25 18:31:12 +0200

    Initial Commit

---
+++ HUDKitDemo/HUDKitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  HUDKitDemoApp.swift
+//  HUDKitDemo
+//
+//  Created by Alex Nagy on 25.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct HUDKitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
